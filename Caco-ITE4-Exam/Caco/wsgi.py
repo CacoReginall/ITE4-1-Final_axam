91746dd9c9919eb6a28db4a3c27033b7e9cf6468
CacoReginall <119414013+CacoReginall@users.noreply.github.com>
2023-01-12 14:33:51 +0800

    Update wsgi.py

--- Caco-ITE4-Exam/Caco/wsgi.py
+++ Caco-ITE4-Exam/Caco/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rockpaperscissors.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Caco.settings')
 
 application = get_wsgi_application()
